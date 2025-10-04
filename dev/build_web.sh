648f593164097b5693f352d11c1f87a9029d2161
lilly <li@lly.sh>
2025-10-04 22:16:22 +0200

    configure web export

---
+++ dev/build_web.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+D=$(realpath $(dirname $(dirname $0)))
+
+exec docker run -i --rm -v $D:/usr/local/src/ldjam58 docker.io/rust <<EOF
+  cd /usr/local/src/ldjam58
+  rustup target add wasm32-unknown-unknown
+  cargo install wasm-bindgen-cli
+  cargo install --git https://github.com/TheBevyFlock/bevy_cli --locked bevy_cli
+  bevy build web --bundle
+EOF
