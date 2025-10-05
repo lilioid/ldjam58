a17b7b71beb2234ffd1a3dba1e8b266422ed0a87
lilly <li@lly.sh>
2025-10-05 16:13:57 +0200

    make build_web.sh build release assets

--- dev/build_web.sh
+++ dev/build_web.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 D=$(realpath $(dirname $(dirname $0)))
 
-exec docker run -i --rm -v $D:/usr/local/src/ldjam58 docker.io/rust <<EOF
+exec docker run -i --rm \
+  -v ldjam58-cargo-cache:/usr/local/cargo \
+  -v $D:/usr/local/src/ldjam58 docker.io/rust \
+  <<EOF
   cd /usr/local/src/ldjam58
   rustup target add wasm32-unknown-unknown
   cargo install wasm-bindgen-cli
+  cargo install wasm-opt
   cargo install --git https://github.com/TheBevyFlock/bevy_cli --locked bevy_cli
-  bevy build web --bundle
+  bevy build --release web --bundle -v
 EOF
